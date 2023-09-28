3ef00b91ba8e039bc5c1c9105931157714e9e0b0
jrxcjr <jrxcavalcantijr@hotmail.com>
2023-09-28 18:44:11 -0300

    added receiver to deck file

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 // this is a declaration of a new type.
 // create a new type of 'deck'
 type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
