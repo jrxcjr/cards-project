3f951a34c69ecd04f87de0b129e1755ab98a19ce
jrxcjr <jrxcavalcantijr@hotmail.com>
2023-09-28 19:39:04 -0300

    added method to create a deck of cards

--- cards/deck.go
+++ cards/deck.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// this is a declaration of a new type.
-// create a new type of 'deck'
 type deck []string
+
+// This function creates a new deck.
+// This does not need a receiver due to the fact
+// That this is creating a new object. By convention, it does not make a lot of sense to do so.
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "King", "Queen"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
 
 // This function prints all of the items in a deck
 func (d deck) print() {
