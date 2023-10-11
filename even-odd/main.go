1dc318fd7b39340b7125bd66163e62e84e37cc80
jrxcjr <jrxcavalcantijr@hotmail.com>
2023-10-11 08:45:08 -0300

    removed even-odd lesson from cards

--- even-odd/main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// create a newe package
-// in the main function, create a slice of ints from 0 through 10
-// iterate through slice. for each element, check to see if number is even or odd.
-// if value is even, print out "even". if it is odd, print out "odd"
